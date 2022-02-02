4243122829ea3b48e4d5ad3feae4489779a08ab7
Leonardo P Goss <goss.leonardo3@gmail.com>
2022-02-02 10:36:49 -0600

    Add speakers array and elementGenerator function to index.js
    I also removed the speakers cards in index.html

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,23 @@
+const speakers = [
+  {
+    name: "Amanda Oliveira",
+    about: 'Professor of Elderly Care at University of Sao Paulo',
+    moreInfo: 'Amanda, author of the best-seller: "Learning from the aged ones", is a reference to using storytelling to improve the relationship with the elderly.',
+    img: 'images/speakers/avatar-amanda.jpg',
+    altImg: ''
+  }
+]
 
+// create elements and add them class
+function elementGenerator(type, classes) {
+  const elem = document.createElement(type)
+  
+  const classArr = classes.split(" ")
+  classArr.forEach(cl => {
+    elem.classList.add(cl)
+  })
+
+  return elem
+}
+
+console.log(elementGenerator('div', 'test test'))

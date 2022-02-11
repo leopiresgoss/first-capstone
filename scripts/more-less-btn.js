e70b610125496aa86ffdc48b7a7ab5fd96763412
Leonardo P Goss <goss.leonardo3@gmail.com>
2022-02-11 15:31:21 -0600

    Add script to update speakers list when resize the page
    I also fixed some lint errors

--- scripts/more-less-btn.js
+++ scripts/more-less-btn.js
@@ -1,6 +1,6 @@
 function checkWindowWidth() {
   // check if matches for mobile version
-  const width = window.matchMedia("(max-width: 768px)");
+  const width = window.matchMedia('(max-width: 768px)');
   return width.matches;
 }
 
@@ -11,57 +11,50 @@ function reduceSpeakers() {
     if (index > 1) {
       speaker.classList.replace('d-flex', 'd-none');
     }
-  })
+  });
 }
 
 function appearAllSpeakers() {
   const speakers = document.querySelectorAll('#speakers .cards ul li');
   speakers.forEach((speaker) => {
     speaker.classList.replace('d-none', 'd-flex');
-  })
-}
-
-function lessBtn() {
-  const less = document.querySelector('.more-less button');
-  less.ariaLabel = "Close";
-  less.innerHTML = 'Less<i class="fas fa-chevron-up"></i>';
-
-  less.addEventListener('click', () => {
-    reduceSpeakers();
-    moreBtn();
-  })
+  });
 }
 
 function moreBtn() {
   const more = document.querySelector('.more-less button');
-  more.ariaLabel = "More Speakers";
+  more.ariaLabel = 'More Speakers';
   more.innerHTML = 'More<i class="fas fa-chevron-down"></i>';
 
   more.addEventListener('click', () => {
     appearAllSpeakers();
     lessBtn();
-  })
+  });
 }
 
+function lessBtn() {
+  const less = document.querySelector('.more-less button');
+  less.ariaLabel = 'Close';
+  less.innerHTML = 'Less<i class="fas fa-chevron-up"></i>';
+
+  less.addEventListener('click', () => {
+    reduceSpeakers();
+    moreBtn();
+  });
+}
 
 document.addEventListener('DOMContentLoaded', () => {
-  if(checkWindowWidth()) {
+  if (checkWindowWidth()) {
     reduceSpeakers();
     moreBtn();
   }
-})
+});
 
-
-
-// Fix when user scroll page late
-
-// document.addEventListener('scroll', () => {
-//   if(checkWindowWidth()) {
-//     reduceSpeakers();
-//   } else {
-//     appearAllSpeakers();
-//   }
-// })
-
-
-
+window.addEventListener('resize', () => {
+  if (checkWindowWidth()) {
+    reduceSpeakers();
+    moreBtn();
+  } else {
+    appearAllSpeakers();
+  }
+});

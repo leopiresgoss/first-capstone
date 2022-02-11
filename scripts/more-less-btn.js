17b0ca359804c8c79740721c103dd3912f2f72f5
Leonardo P Goss <goss.leonardo3@gmail.com>
2022-02-11 15:42:33 -0600

    Update more-less-bt file using class

--- scripts/more-less-btn.js
+++ scripts/more-less-btn.js
@@ -1,60 +1,64 @@
-function checkWindowWidth() {
-  // check if matches for mobile version
-  const width = window.matchMedia('(max-width: 768px)');
-  return width.matches;
+class MoreLessBtn {
+  checkWindowWidth = () => {
+    // check if matches for mobile version
+    const width = window.matchMedia('(max-width: 768px)');
+    return width.matches;
+  }
+
+  reduceSpeakers = () => {
+    // let only two speakers visible
+    const speakers = document.querySelectorAll('#speakers .cards ul li');
+    speakers.forEach((speaker, index) => {
+      if (index > 1) {
+        speaker.classList.replace('d-flex', 'd-none');
+      }
+    });
+  }
+
+  appearAllSpeakers = () => {
+    const speakers = document.querySelectorAll('#speakers .cards ul li');
+    speakers.forEach((speaker) => {
+      speaker.classList.replace('d-none', 'd-flex');
+    });
+  }
+
+  moreBtn = () => {
+    const more = document.querySelector('.more-less button');
+    more.ariaLabel = 'More Speakers';
+    more.innerHTML = 'More<i class="fas fa-chevron-down"></i>';
+
+    more.addEventListener('click', () => {
+      this.appearAllSpeakers();
+      this.lessBtn();
+    });
+  }
+
+  lessBtn = () => {
+    const less = document.querySelector('.more-less button');
+    less.ariaLabel = 'Close';
+    less.innerHTML = 'Less<i class="fas fa-chevron-up"></i>';
+
+    less.addEventListener('click', () => {
+      this.reduceSpeakers();
+      this.moreBtn();
+    });
+  }
 }
 
-function reduceSpeakers() {
-  // let only two speakers visible
-  const speakers = document.querySelectorAll('#speakers .cards ul li');
-  speakers.forEach((speaker, index) => {
-    if (index > 1) {
-      speaker.classList.replace('d-flex', 'd-none');
-    }
-  });
-}
-
-function appearAllSpeakers() {
-  const speakers = document.querySelectorAll('#speakers .cards ul li');
-  speakers.forEach((speaker) => {
-    speaker.classList.replace('d-none', 'd-flex');
-  });
-}
-
-function moreBtn() {
-  const more = document.querySelector('.more-less button');
-  more.ariaLabel = 'More Speakers';
-  more.innerHTML = 'More<i class="fas fa-chevron-down"></i>';
-
-  more.addEventListener('click', () => {
-    appearAllSpeakers();
-    lessBtn();
-  });
-}
-
-function lessBtn() {
-  const less = document.querySelector('.more-less button');
-  less.ariaLabel = 'Close';
-  less.innerHTML = 'Less<i class="fas fa-chevron-up"></i>';
-
-  less.addEventListener('click', () => {
-    reduceSpeakers();
-    moreBtn();
-  });
-}
+const moreLess = new MoreLessBtn();
 
 document.addEventListener('DOMContentLoaded', () => {
-  if (checkWindowWidth()) {
-    reduceSpeakers();
-    moreBtn();
+  if (moreLess.checkWindowWidth()) {
+    moreLess.reduceSpeakers();
+    moreLess.moreBtn();
   }
 });
 
 window.addEventListener('resize', () => {
-  if (checkWindowWidth()) {
-    reduceSpeakers();
-    moreBtn();
+  if (moreLess.checkWindowWidth()) {
+    moreLess.reduceSpeakers();
+    moreLess.moreBtn();
   } else {
-    appearAllSpeakers();
+    moreLess.appearAllSpeakers();
   }
 });
